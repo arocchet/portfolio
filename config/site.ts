039b0cdb29d137b4f92fd729558e148392430831
arocchet <adrieen.rocchetti@gmail.com>
2025-01-24 18:13:55 +0100

    change framework to next ui

---
+++ config/site.ts
@@ -0,0 +1,12 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "Adrien Rocchetti",
+  description: "Mon portfolio.",
+  navItems: [],
+  navMenuItems: [],
+  links: {
+    github: "https://github.com/arocchet",
+    discord: "https://discordapp.com/users/331333861340479488",
+  },
+};
