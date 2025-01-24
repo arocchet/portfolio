bd1fe0b9e533cf624f932dbce0fd8661ba2d9f7e
arocchet <adrieen.rocchetti@gmail.com>
2025-01-24 16:17:52 +0100

    add theme providers & toggle theme button, change font

---
+++ src/components/theme-toggle.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import * as React from "react";
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
+
+import { Button } from "@/components/ui/button";
+
+export function ModeToggle() {
+  const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = React.useState(false);
+
+  // Utilisez un useEffect pour garantir que le code ne sera exécuté qu'après le premier rendu côté client
+  React.useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  // Ne pas rendre le composant avant que le code ne soit monté côté client
+  if (!mounted) {
+    return (
+      <Button variant="outline" size="icon" className="relative">
+        {theme === "dark" ? (
+          <Moon className="h-[1.2rem] w-[1.2rem]" />
+        ) : (
+          <Sun className="h-[1.2rem] w-[1.2rem]" />
+        )}
+        <span className="sr-only">Toggle theme</span>
+      </Button>
+    );
+  }
+
+  // Fonction pour basculer entre les modes
+  const toggleTheme = () => {
+    if (theme === "dark") {
+      setTheme("light");
+    } else {
+      setTheme("dark");
+    }
+  };
+
+  return (
+    <Button
+      variant="outline"
+      size="icon"
+      onClick={toggleTheme}
+      className="relative"
+    >
+      {theme === "dark" ? (
+        <Moon className="h-[1.2rem] w-[1.2rem]" />
+      ) : (
+        <Sun className="h-[1.2rem] w-[1.2rem]" />
+      )}
+      <span className="sr-only">Toggle theme</span>
+    </Button>
+  );
+}
