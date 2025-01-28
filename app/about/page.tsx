cc1f45731ce2bff940f222e6991985e42509be24
arocchet <adrieen.rocchetti@gmail.com>
2025-01-28 23:32:52 +0100

    add timeline && about me page started

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,4 +1,4 @@
-import { title } from "@/components/primitives";
+import CustomizedTimeline from "@/components/custom-timeline";
 
 export default function AboutPage() {
   return (
@@ -6,8 +6,51 @@ export default function AboutPage() {
       <main className="container mx-auto flex flex-col items-center justify-center px-8 py-16 sm:py-24">
         <section className="flex flex-col items-center justify-center gap-8 sm:gap-6">
           <h1 className="text-center text-4xl sm:text-5xl font-bold leading-tight text-default-700">
-            About me...
+            Me
           </h1>
+          <p className="text-lg sm:text-xl text-default-500 max-w-3xl">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Repellendus, aliquid, iusto quia aperiam quo quos non enim ut animi
+            ipsam praesentium quaerat cupiditate nobis aliquam maiores ullam
+            rerum atque tempora.
+          </p>
+          <p className="text-lg sm:text-xl text-default-500 max-w-3xl">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est esse
+            omnis itaque adipisci officiis, optio, voluptates placeat non sunt
+            odit, tenetur ex tempore perferendis nemo iusto quas nulla iure
+            mollitia!{" "}
+          </p>
+          <h1 className="text-center text-4xl sm:text-5xl font-bold leading-tight text-default-700">
+            Education
+          </h1>
+          <CustomizedTimeline />
+          <p className="text-lg sm:text-xl text-default-500 max-w-3xl">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Repellendus, aliquid, iusto quia aperiam quo quos non enim ut animi
+            ipsam praesentium quaerat cupiditate nobis aliquam maiores ullam
+            rerum atque tempora.
+          </p>
+          <p className="text-lg sm:text-xl text-default-500 max-w-3xl">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est esse
+            omnis itaque adipisci officiis, optio, voluptates placeat non sunt
+            odit, tenetur ex tempore perferendis nemo iusto quas nulla iure
+            mollitia!{" "}
+          </p>
+          <h1 className="text-center text-4xl sm:text-5xl font-bold leading-tight text-default-700">
+            Title...
+          </h1>
+          <p className="text-lg sm:text-xl text-default-500 max-w-3xl">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Repellendus, aliquid, iusto quia aperiam quo quos non enim ut animi
+            ipsam praesentium quaerat cupiditate nobis aliquam maiores ullam
+            rerum atque tempora.
+          </p>
+          <p className="text-lg sm:text-xl text-default-500 max-w-3xl">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est esse
+            omnis itaque adipisci officiis, optio, voluptates placeat non sunt
+            odit, tenetur ex tempore perferendis nemo iusto quas nulla iure
+            mollitia!{" "}
+          </p>
         </section>
       </main>
     </div>
