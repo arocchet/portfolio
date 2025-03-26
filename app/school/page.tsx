79a5ae198bef4ef77510766a5b24bfe5c3f0ea5d
arocchet <adrieen.rocchetti@gmail.com>
2025-03-26 11:06:27 +0100

    School text updated 2

--- app/school/page.tsx
+++ app/school/page.tsx
@@ -18,7 +18,7 @@ export default function SchoolPage() {
             I am currently enrolled in a two-year training program to become a software developer, with a focus on solving real-world projects. This program allows me to acquire a comprehensive set of skills in development.
           </p>
           <p className="text-lg sm:text-xl text-default-500 max-w-3xl">
-            The goal is to train as a full-stack developer in 2 to 3 years, through work-study. From design to production of a project or application, I work with different programming languages. The approach is agile and collaborative, transforming me from a beginner to a developer of complete software. By completing more than 55 projects and 250 code audits. I'm learning to code in JavaScript, Go, Rust, as well as in C, C++, Java, Python, and many more. This allows of coding in any programming environment.
+            The goal is to train as a full-stack developer in 3 years, through work-study. From design to production of a project or application, I work with different programming languages. The approach is agile and collaborative, transforming me from a beginner to a developer of complete software. By completing more than 55 projects and 250 code audits. I'm learning to code in JavaScript, Go, Rust, as well as in C, C++, Java, Python, and many more. This allows of coding in any programming environment.
           </p>
           <h1 className="text-center text-4xl sm:text-5xl font-bold leading-tight text-default-700">
             The program consists of:
