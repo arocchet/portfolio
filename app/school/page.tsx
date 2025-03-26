1c9d488502988a72309543a534635066be25fbc0
arocchet <adrieen.rocchetti@gmail.com>
2025-03-26 11:04:11 +0100

    School text updated

--- app/school/page.tsx
+++ app/school/page.tsx
@@ -7,7 +7,7 @@ import React from "react";
 
 export default function SchoolPage() {
   return (
-    <div className="relative flex h-full w-full flex-col bg-background dark:bg-black-90 light:bg-white-90 text-justify hyphens-auto">
+    <div className="relative flex h-full w-full flex-col bg-background dark:bg-black-90 light:bg-white-90 text-justify hyphens-auto leading-relaxed">
       <main className="container mx-auto flex flex-col items-center justify-center px-8 py-16 sm:py-24">
         {/* Introduction */}
         <section className="flex flex-col items-center justify-center gap-8 sm:gap-6">
@@ -18,14 +18,14 @@ export default function SchoolPage() {
             I am currently enrolled in a two-year training program to become a software developer, with a focus on solving real-world projects. This program allows me to acquire a comprehensive set of skills in development.
           </p>
           <p className="text-lg sm:text-xl text-default-500 max-w-3xl">
-            The goal is to train as a full-stack developer in 2 to 3 years, through work-study. From design to production of a project or application, I work with different programming languages. The approach is agile and collaborative, transforming me from a beginner to a developer of complete software. By completing more than 55 projects, I learn to code in JavaScript, Go, Rust, as well as in C, C++, Java, Python, and many more. This makes me capable of coding in any programming environment.
+            The goal is to train as a full-stack developer in 2 to 3 years, through work-study. From design to production of a project or application, I work with different programming languages. The approach is agile and collaborative, transforming me from a beginner to a developer of complete software. By completing more than 55 projects and 250 code audits. I'm learning to code in JavaScript, Go, Rust, as well as in C, C++, Java, Python, and many more. This allows of coding in any programming environment.
           </p>
           <h1 className="text-center text-4xl sm:text-5xl font-bold leading-tight text-default-700">
             The program consists of:
           </h1>
           <ul className="text-lg sm:text-xl text-default-500 max-w-3xl list-disc pl-6">
-            <li><strong>Three years of free training</strong> focused on solving real-world projects, which will allow me to acquire a comprehensive set of software development skills.</li>
-            <li><strong>Followed by two years of paid professional experience</strong>, working with one of our partner companies where I will apply the skills I’ve gained.</li>
+            <li><strong>Three years of training</strong> focused on solving real-world projects, which will allow me to acquire a comprehensive set of software development skills.</li>
+            <li><strong>Including two years of professional experience</strong>, within a company where I will put my acquired skills into practice.</li>
           </ul>
           <p className="text-lg sm:text-xl text-default-500 max-w-3xl">
             How do we learn without professors or traditional courses? I will learn through the innovative <strong>01Edu platform</strong> – a peer-to-peer and <DrawerDefinition/> learning platform that has helped thousands of students worldwide become ready-to-work digital talents. By collaborating with other students on group projects, as well as improving my individual skills, I will acquire a complete set of development skills.
